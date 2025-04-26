7985214b65408095699e8277a6bbbe806a93d7dd
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-04-26 18:14:31 +0500

    Добавление категорий товаров

--- SportsStore/Models/ViewModels/ProductsListViewModel.cs
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -4,5 +4,6 @@ namespace SportsStore.Models.ViewModels
     {
         public IEnumerable<Product> Products { get; set; } = Enumerable.Empty<Product>();
         public PagingInfo PagingInfo { get; set; } = new();
+        public string? CurrentCategory { get; set; }
     }
 }
